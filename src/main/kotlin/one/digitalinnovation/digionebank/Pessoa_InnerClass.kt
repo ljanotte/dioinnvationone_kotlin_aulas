8b66cdfd37b96b0d53b84d5e1f6d7f92d9456b87
leandro J. Janotte <ljanotte@gmail.com>
2021-02-21 11:04:48 -0300

    update to apply the private modifiar to set function of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa_InnerClass.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa_InnerClass.kt
@@ -1,14 +1,18 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
+class Pessoa1 {
     var name: String = "Leandro"
     var cpf: String = "034.246.599-67"
-    private  set
+
+    inner class Endereco1 {
+        var rua: String = "Piscis Austrinos"
+    }
 }
 
 fun main() {
-    val leandro = Pessoa()
-    println(leandro)
+    val leandro = Pessoa1()
+
     println(leandro.name)
     println(leandro.cpf)
+    println(leandro.Endereco1().rua)
 }
